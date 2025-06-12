0209efc323b43d91b5f2434e4026ce4dc01d301c
akSkwYX <akskwyx@gmail.com>
2025-06-12 17:20:22 +0200

    Test add of hyprland flake in hypr config instead of nixos-config Add dev shell to make tests for the hypr config

--- flake.nix
+++ flake.nix
@@ -1,7 +1,35 @@
 {
   description = "My Hypr environment configuration as a flake module";
 
-  outputs = { self, ... }: {
+  inputs = {
+    hyprland.url = "github:hyprwm/Hyprland";
+  };
+
+  outputs = { self, hyprland, ... }: {
     homeModules.default = import ./hypr-module.nix;
+    hyprpkg = hyprland.packages."x86_64-linux".hyprland;
+
+    devShells.default.hypr = pkgs.mkShell {
+      buildInputs = [
+        hyprpkg
+        hyprpaper
+        hyprlock
+        hypridle
+        eww
+        alsa-utils
+        libnotify
+        swaynotificationcenter
+        rofi-wayland
+        brightnessctl
+        playerctl
+      ];
+      shellHook = ''
+        echo "Entered HyprTest shell"
+        echo "Config root : $HOME/.test"
+        echo "Real config : $HOME/.config"
+        export XDG_CONFIG_HOME="$HOME/.test"
+        mkdir -p "$XDG_CONFIG_HOME"/{hypr,rofi,eww}
+      '';
+    };
   };
 }
