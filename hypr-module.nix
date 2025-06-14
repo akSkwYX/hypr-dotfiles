0bc1e2194ad0ec6d7e098975b3ecc7eb4a0966da
akSkwYX <akskwyx@gmail.com>
2025-06-14 15:35:21 +0200

    Remove rofi and eww config

--- hypr-module.nix
+++ hypr-module.nix
@@ -9,8 +9,6 @@ in
 {
   imports = [
     ./conf/hypr/scripts/scripts.nix
-    ./conf/rofi/scripts/scripts.nix
-    ./conf/eww/scripts/scripts.nix
     ./conf/quickshell/scripts/scripts.nix
   ];
 
@@ -40,16 +38,6 @@ in
   home.file = {
     ".config/hypr" = {
       source = ./conf/hypr/confFiles;
-      recursive = true;
-    };
-
-    ".config/rofi" = {
-      source = ./conf/rofi/confFiles;
-      recursive = true;
-    };
-
-    ".config/eww" = {
-      source = ./conf/eww/confFiles;
       recursive = true;
     };
 
