7c231108a36b6838dbaa84d810ed1ded8b1366b4
akSkwYX <akskwyx@gmail.com>
2025-06-18 12:20:16 +0200

    Fix 1password startup silent + Fix quickshell conf file

--- conf/quickshell/quickshell.nix
+++ conf/quickshell/quickshell.nix
@@ -1,43 +1,9 @@
 { config, pkgs, lib, ... }:
 {
   home.file = {
-    ".config/quickshell/assets" = {
-      source = ./confFiles/assets;
+    ".config/quickshell"  = {
+      source = ./confFiles;
       recursive = true;
-    };
-
-    ".config/quickshell/config" = {
-      source = ./confFiles/config;
-      recursive = true;
-    };
-
-    ".config/quickshell/data" = {
-      source = ./confFiles/data;
-      recursive = true;
-    };
-
-    ".config/quickshell/modules" = {
-      source = ./confFiles/modules;
-      recursive = true;
-    };
-
-    ".config/quickshell/services" = {
-      source = ./confFiles/services;
-      recursive = true;
-    };
-
-    ".config/quickshell/utils" =  {
-      source = ./confFiles/utils;
-      recursive = true;
-    };
-
-    ".config/quickshell/widgets" = {
-      source = ./confFiles/widgets;
-      recursive = true;
-    };
-
-    ".config/quickshell/shell.qml" = {
-      source = ./confFiles/shell.qml;
     };
   };
 }
