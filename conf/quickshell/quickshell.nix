de86c1766247aed80c25d5e2d623332c4b0cc441
akSkwYX <akskwyx@gmail.com>
2025-06-18 12:04:51 +0200

    Add 1password start on boot

--- conf/quickshell/quickshell.nix
+++ conf/quickshell/quickshell.nix
@@ -1,8 +1,42 @@
 { config, pkgs, lib, ... }:
 {
   home.file = {
-    ".config/quickshell" = {
-      source = ./confFiles;
+    ".config/quickshell/assets" = {
+      source = ./confFiles/assets;
+      recursive = true;
+    };
+
+    ".config/quickshell/config" = {
+      source = ./confFiles/config;
+      recursive = true;
+    };
+
+    ".config/quickshell/data" = {
+      source = ./confFiles/data;
+      recursive = true;
+    };
+
+    ".config/quickshell/modules" = {
+      source = ./confFiles/modules;
+      recursive = true;
+    };
+
+    ".config/quickshell/services" = {
+      source = ./confFiles/utils;
+      recursive = true;
+    };
+
+    ".config/quickshell/widgets" = {
+      source = ./confFiles/widgets;
+      recursive = true;
+    };
+
+    ".config/quickshell/shell.qml" = {
+      source = ./confFiles/shell.qml;
+    };
+
+    ".config/quickshell/config" = {
+      source = ./confFiles/config;
       recursive = true;
     };
   };
